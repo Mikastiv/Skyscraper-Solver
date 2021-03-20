c68650d42403efa6f78d60230520e5cbb218b899
Samuel Leclerc <samleclerc30@gmail.com>
2021-03-20 16:44:57 -0400

    Version 2

---
+++ ex00/input_validation.h
@@ -0,0 +1,69 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   input_validation.h                                 :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sleclerc <sleclerc@student.42quebec>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/03/20 11:47:53 by sleclerc          #+#    #+#             */
+/*   Updated: 2021/03/20 14:57:46 by sleclerc         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef INPUT_VALIDATION_H
+#define INPUT_VALIDATION_H
+
+int	ft_is_space(char c)
+{
+	if (c == ' ')
+		return (1);
+	else
+		return (0);
+}
+
+int	ft_is_valid_num(char c)
+{
+	if (c >= '1' && c < '1' + N)
+		return (1);
+	else
+		return (0);
+}
+
+int	ft_input_is_valid(char *input)
+{
+	int	count;
+	int	prev_is_num;
+
+	prev_is_num = 0;
+	count = 0;
+	while (*input != '\0' && count != 4 * N)
+	{
+		if (!prev_is_num && ft_is_valid_num(*input))
+			count++;
+		else if (prev_is_num && ft_is_space(*input))
+		{
+		}
+		else
+			return (0);
+		prev_is_num = !prev_is_num;
+		input++;
+	}
+	if (count == 4 * N && *input == '\0')
+		return (1);
+	return (0);
+}
+
+void	ft_parse_input(int viewers[], char *input)
+{
+	int	i;
+	
+	i = 0;
+	while (i < 4 * N)
+	{
+		viewers[i] = *input - '0';
+		i++;
+		input += 2;
+	}
+}
+
+#endif
--- ex00/main.c
+++ ex00/main.c
@@ -6,14 +6,14 @@
 /*   By: mleblanc <mleblanc@student.42quebec>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/20 09:37:04 by mleblanc          #+#    #+#             */
-/*   Updated: 2021/03/20 11:00:12 by mleblanc         ###   ########.fr       */
+/*   Updated: 2021/03/20 16:42:16 by sleclerc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#define N 4
 #include <unistd.h>
 #include <stdio.h>
-
-#define N 4
+#include "input_validation.h"
 
 void	ft_putstr(char *str)
 {
@@ -24,80 +24,28 @@ void	ft_putstr(char *str)
 	}
 }
 
-int	ft_is_space(char c)
-{
-	if (c == ' ')
-		return (1);
-	else
-		return (0);
+int	ft_2d_to_1d(int row, int col)
+{	
+	return (row * N + col);
 }
 
-int	ft_is_valid_num(char c)
-{
-	if (c >= '1' && c < '1' + N)
-		return (1);
-	else
-		return (0);
-}
-
-int	ft_input_is_valid(char *input)
-{
-	int	count;
-	int	prev_is_num;
-
-	prev_is_num = 0;
-	count = 0;
-	while (*input != '\0' && count != 4 * N)
-	{
-		if (!prev_is_num && ft_is_valid_num(*input))
-			count++;
-		else if (prev_is_num && ft_is_space(*input))
-		{
-		}
-		else
-			return (0);
-		prev_is_num = !prev_is_num;
-		input++;
-	}
-	if (count == 4 * N && *input == '\0')
-		return (1);
-	return (0);
-}
-
-void	ft_parse_input(int viewers[4][N], char *input)
-{
-	int	i;
-	int	j;
-
-	i = 0;
-	while (i < 4)
-	{
-		j = 0;
-		while (*input != '\0' && j < N)
-		{
-			if (*input != ' ')
-			{
-				viewers[i][j] = *input - '0';
-				j++;
-			}
-			input++;
-		}
-		i++;
-	}
-}
-
-void	ft_print_array(int viewers[4][N])
+void	ft_print_array(int array[], int is_result)
 {
 	int i;
 	int j;
+	int k;
 
 	i = 0;
-	while (i < 4)
+	if (is_result)
+		k = N;
+	else
+		k = 4;		
+	while (i < k)
 	{
 		j = 0;
 		while (j < N)
 		{
-			printf("%d ", viewers[i][j]);
+			printf("%d ", array[ft_2d_to_1d(i, j)]);
 			j++;
 		}
 		printf("\n");
@@ -105,9 +53,62 @@ void	ft_print_array(int viewers[4][N])
 	}
 }
 
+void	ft_init_result_array(int array[])
+{
+	int index;
+
+	index = 0;
+	while (index < N * N)
+	{
+		array[index] = 0;
+		index++;	
+	}
+}
+
+int	ft_firstrow_index(int index)
+{
+	while (index > N)
+	{
+		index -= N;
+	}
+	return (index);
+}
+
+int	ft_is_calup_valid(int result[], int col_height, int index, int num)
+{
+	int	box_count;
+	int count;
+	int height;
+	int i;
+
+	box_count = 0;
+	i = ft_firstrow_index(index);
+	count = 0;
+	height = 0;
+	while (i < index)
+	{
+		if (num == result[i])
+			return (0);
+		if (result[i] > height)
+		{
+			height = result[i];
+			count++;
+		}
+		i += N;
+		box_count++;
+	}
+	if (count == col_height && num > height)
+		return (0);
+	if (num < height && box_count == N - 1)
+		return (0);
+	return (1);
+}
+
+
 int	main(int argc, char *argv[])
 {
-	int	viewers[4][N];
+	int	viewers[4 * N];
+	int res[N * N];
 
 	if (argc != 2 || !ft_input_is_valid(argv[1]))
 	{
@@ -115,5 +116,7 @@ int	main(int argc, char *argv[])
 		return (0);
 	}
 	ft_parse_input(viewers, argv[1]);
-	ft_print_array(viewers);
+	ft_print_array(viewers, 0);
+	ft_init_result_array(res);
+	ft_print_array(res, 1);
 }
